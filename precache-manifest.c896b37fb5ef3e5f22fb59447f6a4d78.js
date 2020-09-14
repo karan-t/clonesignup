7c6a1259f686a282a62c120295c774d06a10a2d3
Karan Thakur <karanthakur715@gmail.com>
2020-09-15 00:14:31 +0530

    Updates

---
+++ precache-manifest.c896b37fb5ef3e5f22fb59447f6a4d78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0011f2553c7f6959c1c07b5703e9b50",
+    "url": "/clonesignup/index.html"
+  },
+  {
+    "revision": "c10b9e2b51474c091792",
+    "url": "/clonesignup/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9b944d59da709bd9594c",
+    "url": "/clonesignup/static/css/main.531aeabb.chunk.css"
+  },
+  {
+    "revision": "c10b9e2b51474c091792",
+    "url": "/clonesignup/static/js/2.5fb0208b.chunk.js"
+  },
+  {
+    "revision": "9b944d59da709bd9594c",
+    "url": "/clonesignup/static/js/main.6e672fd2.chunk.js"
+  },
+  {
+    "revision": "340c23e59d41c0dcc44d",
+    "url": "/clonesignup/static/js/runtime-main.67bff9b7.js"
+  },
+  {
+    "revision": "0c0d8c2d73fef5ede69663aa13a2dec7",
+    "url": "/clonesignup/static/media/background.0c0d8c2d.png"
+  }
+]);
